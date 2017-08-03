4e956c209b4cd219e715c5b89ee43b22b49765e7
Nobody <nobody@yq150.onlinetools>
2017-08-03 23:44:54 +0800

    git commit by online edit file 1/webpy.py

--- webpy.py
+++ webpy.py
@@ -3,6 +3,7 @@ import web
 urls =(
     '/.*','hello',
     '/world',"world",
+    '/test','test',
 )
 
 class hello:
@@ -14,6 +15,9 @@ class world:
     def GET(self):
         web.header('Content-Type','text/html; charset=UTF-8')
         return "111111111111111111!"
+class test:
+    def GET(self):
+        return "22222222222222222"
     
 app = web.application(urls,globals())
 if __name__ == '__main__':

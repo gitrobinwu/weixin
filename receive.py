4d43cb14239fac74663ed7ba1f2716b6f078429b
Your Name <wuyongwei@besttone.com.cn>
2017-08-04 16:16:10 +0800

    c

--- receive.py
+++ receive.py
@@ -13,7 +13,7 @@ def parse_xml(web_data):
 	print "444444444444444444444"
 	if msg_type == 'text':
 		print "555555555555555555555"
-		return TextMsg(xmlData)
+		textobj =  TextMsg(xmlData)
 		print "6666666666666666666666"
 	elif msg_type == 'image':
 		return ImageMsg(xmlData)
@@ -28,6 +28,7 @@ class Msg(object):
 
 class TextMsg(Msg):
 	def __init__(self,xmlData):
+		print "888888888888888888888"
 		super(TextMsg,self).__init__(xmlData)
 		self.Content = xmlData.find('Content').text.encode('utf-8')
 

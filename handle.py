033fb3f7e133032050ad1157c6546be92a0d0ef1
Your Name <wuyongwei@besttone.com.cn>
2017-08-06 16:29:48 +0800

    c

--- handle.py
+++ handle.py
@@ -11,7 +11,7 @@ import receive
 class Handle(object):
 	def __init__(self):
 		self.app_root = os.path.dirname(__file__)
-		self.templates_root = os.path.json(self.app_root,'templates')
+		self.templates_root = os.path.join(self.app_root, 'templates')
 		self.render = web.template.render(self.templates_root)
 
 	def GET(self):

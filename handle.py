b634d386742e46aec75d04236c3dd1397c0a438f
Your Name <wuyongwei@besttone.com.cn>
2017-08-06 16:31:39 +0800

    c

--- handle.py
+++ handle.py
@@ -80,11 +80,14 @@ class Handle(object):
 						return self.send_text(toUser,fromUser,content)
 
 				if recMsg.MsgType == "text":
+					print "3333333333333333333333333333"
 					# 文本消息处理
 					content = recMsg.Content
 					if content.find("help") != -1:
+						print "4444444444444444444444"
 						help = u'''1.输入中文或者英文返回对应的英中翻译\n2.输入 book 要查询的书名 返回豆瓣图书中结果\n3.输入cls清除查询记录\n4.输入m随机来首音乐听，建议在wifi下听\n5.输入python 进入python常用模块用法查询（未完成）'''
 						return self.send_text(toUser,fromUser.content)	
+					print "55555555555555555555555555555555555"
 					return self.send_text(toUser,fromUser.content)
 							
 				elif recMsg.MsgType == "image":
